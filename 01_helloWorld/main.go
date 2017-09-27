f1be0943b6102069a27c6206879b1377acfd4975
Matt <mdangelo221@gmail.com>
2017-09-26 20:13:46 -0500

    myUtils

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"github.com/Mattyd221/golangPractice/myUtils"
+	"fmt"
+)
+
 func main() {
+
+	fmt.Println(myUtils.RightPadToLength("0123456789"," ",16))
+	fmt.Println(myUtils.LeftPadToLength("0123456789"," ",16))
 }
---
+++ MyUtils/myUtils.go
@@ -0,0 +1,25 @@
+package myUtils
+
+import "strings"
+
+func LeftPad(startString string, padString string, padLength int) string {
+	return strings.Repeat(padString, padLength) + startString
+}
+
+func RightPad(startString string, padString string, padLength int) string {
+	return startString + strings.Repeat(padString, padLength)
+}
+
+func LeftPadToLength(startString string, padString string, overallLength int) string {
+	var padCount int
+	padCount = 1 + ((overallLength - len(padString))/ len(padString))
+	ans := strings.Repeat(padString, padCount) + startString
+	return ans[(len(ans) - overallLength):]
+}
+
+func RightPadToLength(startString string, padString string, overallLength int) string {
+	var padCount int
+	padCount = 1 + ((overallLength - len(padString))/ len(padString))
+	ans :=  startString + strings.Repeat(padString, padCount)
+	return ans[:overallLength]
+}
